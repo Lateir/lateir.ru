d5f8d76ddfef74aa8c16b30484b4e1c239424a0c
lateir <lfybnz123@gmail.com>
2022-12-20 16:11:15 +0000

    Добавил ноду в сборку образа php, для полноценной работой с ларой, добавил шаблон с сайта bootstrap и минимально его отредактировал под свои нужды. Добавил иконку.

---
+++ www/lateir.ru/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('main_content')
+    <main class="px-3">
+        <h1>Даниил Поветкин</h1>
+        <p class="lead">Backend-программист. Занимаюсь разработкой логики для бизнесс-процессов и сайтов, следующих этой логике.</p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white text-dark">Контакты</a>
+        </p>
+    </main>
+@endsection
+
